7043601103cb0f0de53a97206ba4bc2c7074c57a
Arun Milton <miltononline@gmail.com>
2019-03-05 05:44:20 -0500

    submitting-checkpoint

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -1,56 +1,64 @@
 const inquirer = require('inquirer');
+const ContactController = require("./ContactController");
 
 module.exports = class MenuController {
-  constructor(){
-    this.mainMenuQuestions = [
-      {
-        type: "list",
-        name: "mainMenuChoice",
-        message: "Please choose from an option below: ",
-        choices: [
-          "Add new contact",
-          "Exit"
-        ]
+    constructor(){
+        this.mainMenuQuestions = [
+            {
+             type: "list",
+              name: "mainMenuChoice",
+              message: "Please choose from an option below: ",
+              choices: [
+                "Add new contact",
+                "Exit"
+              ]
+            }
+          ];
+          this.book = new ContactController();
+        }
+
+    main(){
+        console.log(`Welcome to AddressBloc!`);
+        inquirer.prompt(this.mainMenuQuestions).then((response) => {
+          switch(response.mainMenuChoice){
+            case "Add new contact":
+              this.addContact();
+              break;
+            case "Exit":
+              this.exit();
+            default:
+              console.log("Invalid input");
+              this.main();
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
       }
-    ];
-    this.contacts = [];
-  }
 
-  main(){
-    console.log(`Welcome to AddressBloc!`);
-    inquirer.prompt(this.mainMenuQuestions).then((response) => {
-      switch(response.mainMenuChoice){
-        case "Add new contact":
-        this.addContact();
-        break;
-        case "Exit":
-        this.exit();
-        default:
-        console.log("Invalid input");
-        this.main();
+    clear(){
+        console.log("\x1Bc");
+    }
+
+    addContact(){
+        this.clear();
+        inquirer.prompt(this.book.addContactQuestions).then((answers) => {
+          this.book.addContact(answers.name, answers.phone).then((contact) => {
+            console.log("Contact added successfully!");
+            this.main();
+          }).catch((err) => {
+            console.log(err);
+            this.main();
+          });
+        });
       }
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  }
 
-  clear(){
-    console.log("\x1Bc");
-  }
+      getContactCount(){
+        return this.contacts.length;
+      }
 
-  addContact(){
-    this.clear();
-    console.log("addContact called");
-    this.main();
-  }
-
-  exit(){
-    console.log("Thanks for using AddressBloc!");
-    process.exit();
-  }
-
-  getContactCount(){
-    return this.contacts.length;
-  }
-}
+      exit(){
+        console.log("Thanks for using AddressBloc!");
+        process.exit();
+      }
+    }
